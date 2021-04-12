1378cf314d6c3bba812220e496ccac6539a3e7b9
Axel Dhaussy <axel.dhaussy@edu.ece.fr>
2021-04-12 19:48:43 +0200

    page2

--- page1.js
+++ page1.js
@@ -5,3 +5,10 @@ function confirmer(){
     else
     	return false;
 }
+
+function pictureChange(){
+	document.body.style.backgroundImage = "url('images/pyramid_close.jpg')";
+}
+function pictureChange2(){
+	document.body.style.backgroundImage = "url('images/pyramid_close2.jpg')";
+}
