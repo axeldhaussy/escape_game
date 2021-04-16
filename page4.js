c79b9eb9d54a391739aa40b4fc6889f59b499bbd
Axel Dhaussy <axel.dhaussy@edu.ece.fr>
2021-04-16 17:42:58 +0200

    page4 ok

---
+++ page4.js
@@ -0,0 +1,7 @@
+function leftDoor(){
+    window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page3.html";
+}
+
+function rightDoor(){
+    window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page2.html";
+}
