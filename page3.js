c3f5e47304a1a60da67d3656814932f860b2a3fe
Axel Dhaussy <axel.dhaussy@edu.ece.fr>
2021-04-16 19:49:32 +0200

    add page5

--- page3.js
+++ page3.js
@@ -4,7 +4,7 @@ var max = 3;
 
 function check(){
 	var str = document.getElementById("answer").value;
-	if((str.includes("1000001")) && (nb < max)){
+	if((str == 1000001) && (nb < max)){
         alert("CORRECT!");
         window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page4.html";
         document.getElementById("checkbutton").style.display = "none";
@@ -31,29 +31,72 @@ function check(){
 
 		start();
     }
+    document.getElementById("triesLeft").innerHTML = result;
+}
+function check2(){
+	var str = document.getElementById("answer").value;
+	if((str == 13) && (nb < max)){
+        alert("CORRECT!");
+        window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page4.html";
+        document.getElementById("checkbutton").style.display = "none";
+        document.getElementById("answer").style.display = "none";
+        document.getElementById("help").style.display = "none";
+     	document.getElementById("idea").style.display = "none";
+    } else{
+    	var result = max - nb - 1;
+        alert("WRONG! " + " Remaining tries: " + result);
+        nb ++;
+    }
+    if(nb == 3){
+    	alert("Sorry it was your last chance... The pyramid got you. You will now go to jail for 15 seconds before trying again");
+    	document.body.style.backgroundImage = "url('images/jail.png')";
+    	document.body.style.backgroundPosition = "300px -150px";
+    	document.body.style.backgroundColor = "grey";
+    	document.getElementById("triesLeft").style.display = "none";
+    	document.getElementById("triesR").style.display = "none";
+    	document.getElementById("div1").style.display = "none";
+     	document.getElementById("exit").style.display = "none";
+    	document.getElementById("checkbutton").style.display = "none";
+		document.getElementById("answer").style.display = "none";
+		document.getElementById("bip").style.display = "block";
+		document.getElementById("bip1").style.display = "block";
+
+		start2();
+    }
+    document.getElementById("triesLeft").innerHTML = result;
 }
 
 //timer
 var counter = 15;
 var intervalId = null;
+var intervalId2 = null;
 
-function finish() {
+
+function finish(){
   clearInterval(intervalId);
   nb = 0;
   counter = 15;
 }
-function bip() {
+function finish2(){
+  clearInterval(intervalId2);
+  nb = 0;
+  counter = 15;
+}
+function bip(){
     counter--;
     if(counter == 0){
     	finish();
-    	document.body.style.backgroundImage = "url('images/fond_game.jpg')";
-    	document.body.style.backgroundColor = "orange";
-    	document.body.style.backgroundPosition = "330px -70px";
-    	document.getElementById("exit").style.display = "block";
-    	document.getElementById("checkbutton").style.display = "block";
-		document.getElementById("answer").style.display = "block";
-		document.getElementById("bip").style.display = "none";
-		document.getElementById("bip1").style.display = "none";
+    	window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page3.html";
+    } 
+    else {	
+        document.getElementById("bip").innerHTML = counter + " seconds";
+    }	
+}
+function bip2(){
+    counter--;
+    if(counter == 0){
+    	finish2();
+    	window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page5.html";
     } 
     else {	
         document.getElementById("bip").innerHTML = counter + " seconds";
@@ -62,6 +105,9 @@ function bip() {
 //start timer
 function start(){
   intervalId = setInterval(bip, 1000);
+}
+function start2(){
+  intervalId2 = setInterval(bip2, 1000);
 }
 
 //help button
--- page4.js
+++ page4.js
@@ -1,7 +1,7 @@
 function leftDoor(){
-    window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page3.html";
+    window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page5.html";
 }
 
 function rightDoor(){
-    window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page2.html";
+    window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page5b.html";
 }
