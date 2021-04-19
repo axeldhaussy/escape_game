2803899e6d13ec17294aaa43c45bb305b3978dce
Axel Dhaussy <axel.dhaussy@edu.ece.fr>
2021-04-19 14:06:44 +0200

    add page5b ok

--- page3.js
+++ page3.js
@@ -28,6 +28,8 @@ function check(){
 		document.getElementById("bip1").style.display = "block";
 		document.getElementById("help").style.display = "none";
      	document.getElementById("idea").style.display = "none";
+        document.getElementById("triesLeft").style.display = "none";
+        document.getElementById("triesR").style.display = "none";
 
 		start();
     }
@@ -66,10 +68,40 @@ function check2(){
     document.getElementById("triesLeft").innerHTML = result;
 }
 
+function check3(){
+    var str = document.getElementById("answer").value;
+    if((str == 27) && (nb < max)){
+        alert("CORRECT!");
+        window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page4.html";
+        document.getElementById("checkbutton").style.display = "none";
+        document.getElementById("answer").style.display = "none";
+    } else{
+        var result = max - nb - 1;
+        alert("WRONG! " + " Remaining tries: " + result);
+        nb ++;
+    }
+    if(nb == 3){
+        alert("Sorry it was your last chance... The pyramid got you. You will now go to jail for 15 seconds before trying again");
+        document.body.style.backgroundImage = "url('images/jail.png')";
+        document.body.style.backgroundPosition = "300px -150px";
+        document.body.style.backgroundColor = "grey";
+        document.getElementById("triesLeft").style.display = "none";
+        document.getElementById("triesR").style.display = "none";
+        document.getElementById("div1").style.display = "none";
+        document.getElementById("exit").style.display = "none";
+        document.getElementById("checkbutton").style.display = "none";
+        document.getElementById("answer").style.display = "none";
+        document.getElementById("bip").style.display = "block";
+        document.getElementById("bip1").style.display = "block";
+
+        start3();
+    }
+    document.getElementById("triesLeft").innerHTML = result;
+}
+
 //timer
 var counter = 15;
 var intervalId = null;
-var intervalId2 = null;
 
 
 function finish(){
@@ -77,11 +109,7 @@ function finish(){
   nb = 0;
   counter = 15;
 }
-function finish2(){
-  clearInterval(intervalId2);
-  nb = 0;
-  counter = 15;
-}
+
 function bip(){
     counter--;
     if(counter == 0){
@@ -95,12 +123,22 @@ function bip(){
 function bip2(){
     counter--;
     if(counter == 0){
-    	finish2();
+    	finish();
     	window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page5.html";
     } 
     else {	
         document.getElementById("bip").innerHTML = counter + " seconds";
     }	
+}
+function bip3(){
+    counter--;
+    if(counter == 0){
+        finish();
+        window.location.href="file:///Users/axel/Desktop/Web%20Technologies/Projet/escape_game/page5b.html";
+    } 
+    else {  
+        document.getElementById("bip").innerHTML = counter + " seconds";
+    }   
 }
 //start timer
 function start(){
@@ -108,6 +146,9 @@ function start(){
 }
 function start2(){
   intervalId2 = setInterval(bip2, 1000);
+}
+function start3(){
+  intervalId2 = setInterval(bip3, 1000);
 }
 
 //help button
